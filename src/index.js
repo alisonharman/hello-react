394ebce6e06b7455eaa95f2ffadaae3db1dfbbbd
Alison Harman <alison_harman@sympatico.ca>
2022-02-23 10:33:07 -0500

    add input field that will display name onChange

--- src/index.js
+++ src/index.js
@@ -4,23 +4,28 @@ import './index.css';
 
 const Button = (props) => {
   return (
-    <button>{props.children}</button>
+    <button onClick={props.onClick} >{props.children}</button>
   );
 };
 
 const Application = () => {
 
   // your code here
+  const [name, setName] = useState("");
 
   const reset = () => {
-    console.log("reset");
-    // your code here
+    setName("");
   };
 
   return (
     <main>
-      <Button>Reset</Button>
-      <h1>Hello React</h1>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder='Type your name'>
+      </input>
+      <Button onClick={reset}>Reset</Button>
+      {name && <h1>Hello {name}</h1>}
     </main>
   );
 };
